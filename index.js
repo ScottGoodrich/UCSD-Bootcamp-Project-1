59ed957422319dc789bee221e9f937396d91a8de
Jannah June Nieves <jannahjune@gmail.com>
2019-08-09 17:24:25 -0700

    index.js eupdated for main search and artist bio

--- index.js
+++ index.js
@@ -4,61 +4,124 @@ $(document).ready(function () {
         if (event.keyCode === 13) {
             event.preventDefault();
             var input = $("#search").val().trim();
-            var queryURL = "https://rest.bandsintown.com/artists/" + input + "?app_id=codingbootcamp";
-            // var queryURL = "https://en.wikipedia.org/w/api.php?format=json&origin=*&action=query&prop=extracts&exintro&explaintext&redirects=1&titles=" + input;
-            console.log(queryURL);
-            console.log("search made");
-
+            var queryURL = "https://app.ticketmaster.com/discovery/v2/attractions.json?keyword=" + input + "&apikey=xwUI0GkDheBWJnroGGqQsxnOUKENdh4p";
+            console.log("artist button clicked");
             $.ajax({
                 url: queryURL,
-                method: "GET"
+                method: "GET",
+                async: true,
+                dataType: "json",
             }).then(function(response) {
                 console.log(response);
-                //option to create a var and store a new div in it to utilize col-xs-4
-                var artistName = $("<h1>").text(response.name);
-                // var artistName = $("<h1>").text(response.query.pages.pageid.extract);
-                var artistImage = $("<img>").attr("src", response.thumb_url);
-                var artistBIT = $("<a><img src='assets/hand+logo.png' width='100' height='100''>").attr("href", response.url).attr("target", "_blank");
-                var artistFB = $("<a><img src='assets/fb.png' width='100' height='100''>").attr("href", response.facebook_page_url).attr("target", "_blank");
-                var fanCount = $("<h3>").text("Bands in Town Fan Count: " +  response.tracker_count);
-
+                var artistName = $("<h1>").text(input);
+                var artistImage = $("<img>").attr("src", response._embedded.attractions[0].images[0].url).addClass("artistImage");
+                // $("#artist-icons").show();
+    
+                //external link to youtube:
+                if (response._embedded.attractions[0].hasOwnProperty('externalLinks') && response._embedded.attractions[0].externalLinks.hasOwnProperty('youtube')) {
+                    console.log("youtube");
+                    var youTube = response._embedded.attractions[0].externalLinks.youtube[0].url;
+                    var youTubeButton= $("<a><img src='assets/yt.png' width='100' height='100''>").attr("href",youTube).attr("target", "_blank");
+                }
+                // else {
+                //     $("#youtube-link").addClass("unavailable");
+                //     $("#youtube-link").attr("href", "javascript:void(0);");
+                // }
+    
+                //external link to instagram:
+                if (response._embedded.attractions[0].hasOwnProperty('externalLinks') && response._embedded.attractions[0].externalLinks.hasOwnProperty('instagram')) {
+                    console.log("instagram");
+                    var instagram = response._embedded.attractions[0].externalLinks.instagram[0].url;
+                    var instagramButton= $("<a><img src='assets/ig.png' width='100' height='100''>").attr("href",instagram).attr("target", "_blank");
+                }
+                // else {
+                //     $("#instagram-link").addClass("unavailable");
+                //     $("#instagram-link").attr("href", "javascript:void(0);");
+                // }
+    
+                //external link to facebook:
+                if (response._embedded.attractions[0].hasOwnProperty('externalLinks') && response._embedded.attractions[0].externalLinks.hasOwnProperty('facebook')) {
+                    console.log("facebook");
+                    var facebook = response._embedded.attractions[0].externalLinks.facebook[0].url;
+                    var facebookButton= $("<a><img src='assets/fb.png' width='100' height='100''>").attr("href",facebook).attr("target", "_blank");
+                }
+                // else {
+                //     $("#facebook-link").addClass("unavailable");
+                //     $("#facebook-link").attr("href", "javascript:void(0);");
+                // }
                 $("#artist-name").empty();
                 $("#artist-detail").empty();
+                $("#artist-icons").empty();
                 $("#event-info").empty();
+                $("#logo-div").empty();
                 $("#disco-info").empty();
-                $("#lyrics-info").empty();
+                $("#similar-info").empty();
                 $("#artist-name").append(artistName, artistImage);
-                $("#artist-detail").append(artistBIT, artistFB, fanCount);
-                //can we add a jquery function to make css appear when these details append
+                $("#artist-detail").append(youTubeButton, instagramButton, facebookButton);
             })
         }
     })
 
-    $(".artist-button").on("click", function () {
+
+
+
+    $("#artist-button").on("click", function () {
         event.preventDefault();
         var input = $("#search").val().trim();
-        var queryURL = "https://rest.bandsintown.com/artists/" + input + "?app_id=codingbootcamp";
-        console.log("artist bio button clicked");
+        var queryURL = "https://app.ticketmaster.com/discovery/v2/attractions.json?keyword=" + input + "&apikey=xwUI0GkDheBWJnroGGqQsxnOUKENdh4p";
+        console.log("artist button clicked");
         $.ajax({
             url: queryURL,
-            method: "GET"
+            method: "GET",
+            async: true,
+            dataType: "json",
         }).then(function(response) {
             console.log(response);
-            var artistName = $("<h1>").text(response.name);
-            var artistImage = $("<img>").attr("src", response.thumb_url);
-            var artistBIT = $("<a><img src='assets/hand+logo.png' width='100' height='100''>").attr("href", response.url).attr("target", "_blank");
-            var artistFB = $("<a><img src='assets/fb.png' width='100' height='100''>").attr("href", response.facebook_page_url).attr("target", "_blank");
-            var fanCount = $("<h4>").text("Bands in Town Fan Count: " + response.tracker_count);
+            var artistName = $("<h1>").text(input);
+            var artistImage = $("<img>").attr("src", response._embedded.attractions[0].images[0].url).addClass("artistImage");
+            // $("#artist-icons").show();
+
+            //external link to youtube:
+            if (response._embedded.attractions[0].hasOwnProperty('externalLinks') && response._embedded.attractions[0].externalLinks.hasOwnProperty('youtube')) {
+                console.log("youtube");
+                var youTube = response._embedded.attractions[0].externalLinks.youtube[0].url;
+                var youTubeButton= $("<a><img src='assets/yt.png' width='100' height='100''>").attr("href",youTube).attr("target", "_blank");
+            }
+            // else {
+            //     $("#youtube-link").addClass("unavailable");
+            //     $("#youtube-link").attr("href", "javascript:void(0);");
+            // }
+
+            //external link to instagram:
+            if (response._embedded.attractions[0].hasOwnProperty('externalLinks') && response._embedded.attractions[0].externalLinks.hasOwnProperty('instagram')) {
+                console.log("instagram");
+                var instagram = response._embedded.attractions[0].externalLinks.instagram[0].url;
+                var instagramButton= $("<a><img src='assets/ig.png' width='100' height='100''>").attr("href",instagram).attr("target", "_blank");
+            }
+            // else {
+            //     $("#instagram-link").addClass("unavailable");
+            //     $("#instagram-link").attr("href", "javascript:void(0);");
+            // }
+
+            //external link to facebook:
+            if (response._embedded.attractions[0].hasOwnProperty('externalLinks') && response._embedded.attractions[0].externalLinks.hasOwnProperty('facebook')) {
+                console.log("facebook");
+                var facebook = response._embedded.attractions[0].externalLinks.facebook[0].url;
+                var facebookButton= $("<a><img src='assets/fb.png' width='100' height='100''>").attr("href",facebook).attr("target", "_blank");
+            }
+            // else {
+            //     $("#facebook-link").addClass("unavailable");
+            //     $("#facebook-link").attr("href", "javascript:void(0);");
+            // }
             $("#artist-name").empty();
             $("#artist-detail").empty();
+            $("#artist-icons").empty();
             $("#event-info").empty();
+            $("#logo-div").empty();
             $("#disco-info").empty();
-            $("#lyrics-info").empty();
+            $("#similar-info").empty();
             $("#artist-name").append(artistName, artistImage);
-            $("#artist-detail").append(artistBIT, artistFB);
-            $("#artist-detail").append(artistBIT, artistFB, fanCount);
+            $("#artist-detail").append(youTubeButton, instagramButton, facebookButton);
         })
     })
-
-
 })
